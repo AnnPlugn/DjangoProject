233189a50d6d5ae99724a8b767a36855a306ab02
AnnPlugn <a.plygun2005@gmail.com>
2024-04-09 20:54:16 +0300

    с измененной базой на майскл

--- djangoProjectFirst/settings.py
+++ djangoProjectFirst/settings.py
@@ -76,10 +76,6 @@ WSGI_APPLICATION = 'djangoProjectFirst.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
-    'second_db': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django_mysql_db',
         'USER': 'root',
